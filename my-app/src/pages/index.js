fdf1fa651918b1819cd39484b44d0c898bc663ca
rafael santos <rafaelleonagraziele@gmail.com>
2024-05-29 17:41:58 -0300

    teste harry potter

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -2,17 +2,12 @@ import styles from "../styles/Home.module.css";
 import Cards from "@/components/Cards/Cards";
 
 export default function Home() {
+  function handleButtonClick() {}
+
   return (
     <div className={styles.background}>
-      {/* <Image
-        src="/images/fundoHarry.jpg"
-        alt="Fundo Harry Potter"
-        layout="responsive"
-        width={750}
-        height={600}
-      /> */}
       <div className={styles.box}>
-        <button>Veja todos os personagens</button>
+        <button onClick={handleButtonClick}>Veja todos os personagens</button>
       </div>
       <div className={styles.cards}>
         <Cards />
