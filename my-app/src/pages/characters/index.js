05ec11ee93bd9e4d7e21e0108388ff281da5e540
rafael santos <rafaelleonagraziele@gmail.com>
2024-05-31 16:58:41 -0300

    page2

--- my-app/src/pages/characters/index.js
+++ my-app/src/pages/characters/index.js
@@ -1,16 +1,23 @@
 import Image from "next/image";
 import logoHarry from "../../../public/logoHarry.png";
-import styles from "../../styles/Characters.module.css";
+import styles from "../../styles/characters.module.css";
+import LogoMeio from "../../../public/LogoMeio.png";
 
-function characters() {
+function Characters() {
   return (
     <div className={styles.background}>
-      <header className={styles.cabecalho} />
+      <header className={styles.cabecalho}>
+        <div className={styles.logo}>
+          <Image src={logoHarry} alt="logoHarry" />
+        </div>
+      </header>
       <div className={styles.container}>
-        <Image src={logoHarry} alt="logoHarry" />
+        <Image src={LogoMeio} alt="Logo meio" />
+        <p>PERSONAGENS</p>
+        <div className={styles.border} />
       </div>
     </div>
   );
 }
 
-export default characters;
+export default Characters;
